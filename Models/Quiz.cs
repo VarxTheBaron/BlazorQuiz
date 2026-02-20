365e9b29bed4d81280e85545e9705f0f04ac9769
Jonas Persson <jonas.persson111@gmail.com>
2026-02-20 10:54:13 +0100

    bla

--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -1,5 +1,6 @@
 public class Quiz
 {
+    public string Id { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public List<Question> Questions = [];
     public int CurrentQuestionIndex { get; set; }
