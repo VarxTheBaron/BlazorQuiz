d8c82c35f61bcdce21cb907d615607d6b20b0646
Jonas Persson <jonas.persson111@gmail.com>
2026-02-20 10:53:39 +0100

    dictionary

--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -1,7 +1,6 @@
 public class Quiz
 {
-	public string Id { get; set; } = string.Empty;
-	public string Title { get; set; } = string.Empty;
-	public List<Question> Questions = [];
-	public int CurrentQuestionIndex { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public List<Question> Questions = [];
+    public int CurrentQuestionIndex { get; set; }
 }
