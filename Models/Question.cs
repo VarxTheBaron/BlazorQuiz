d758444087e8ca45a01db90deeb8279b168b83d2
biggieliten <filipisaksson00@gmail.com>
2026-02-20 14:20:49 +0100

    öb

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,20 +1,20 @@
 public class Question
 {
-    public string Id { get; set; } = string.Empty;
-    public string Text { get; set; } = string.Empty;
-    public List<string> Answers { get; set; } = [];
-    public int CorrectAnswer { get; set; }
-    public List<ParticipantAnswer> ParticipantAnswers { get; set; } = [];
-    public int[] VoteCount
-    {
-        get
-        {
-            int[] count = [Answers.Count];
-            for (int i = 0; i < count.Length; i++)
-            {
-                count[i] = ParticipantAnswers.Count(pa => pa.AnswerIndex == i);
-            }
-            return count;
-        }
-    }
+	public string Id { get; set; } = string.Empty;
+	public string Text { get; set; } = string.Empty;
+	public List<string> Answers { get; set; } = [];
+	public int CorrectAnswer { get; set; }
+	public List<ParticipantAnswer> ParticipantAnswers { get; set; } = [];
+	public int[] VoteCount
+	{
+		get
+		{
+			int[] count = new int[Answers.Count];
+			for (int i = 0; i < count.Length; i++)
+			{
+				count[i] = ParticipantAnswers.Count(pa => pa.AnswerIndex == i);
+			}
+			return count;
+		}
+	}
 }
