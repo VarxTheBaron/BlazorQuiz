9d30b73e988dac66e500446fb876d2f63e5d8890
biggieliten <filipisaksson00@gmail.com>
2026-02-20 10:41:31 +0100

    add properties for Quiz class

--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -1,4 +1,7 @@
 public class Quiz
 {
-
+	public string Id { get; set; } = string.Empty;
+	public string Title { get; set; } = string.Empty;
+	public List<Question> Questions = [];
+	public int CurrentQuestionIndex { get; set; }
 }
