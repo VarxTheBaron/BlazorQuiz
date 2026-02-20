6ba735f14e3dce9a7daffdb6e1bfe8e764658020
Jonas Persson <jonas.persson111@gmail.com>
2026-02-20 10:22:39 +0100

    first

---
+++ Models/ParticipantAnswer.cs
@@ -0,0 +1,6 @@
+public class ParticipantAnswer
+{
+    public int AnswerIndex { get; set; }
+    public string ParticipantID { get; set; } = string.Empty;
+    public DateTime TimeStamp { get; set; }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,18 @@
+public class Question
+{
+    public string Id { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public List<string> Answers { get; set; } = [];
+    public int CorrectAnswer { get; set; }
+    public List<ParticipantAnswer> ParticipantAnswers { get; set; } = [];
+    // public int[] VoteCount
+    // {
+    //     get => {
+    //         int[] count = new int[ParticipantAnswers.count];
+    //         for (int i = 0; i < ParticipantAnswers.count; i++)
+    //         {
+    //             count[i] = ParticipantAnswers[i]
+    //         }
+    //     }
+    // }
+}
