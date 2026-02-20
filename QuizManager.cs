4499ed5c2f087c3f502b710b9f610ba4f498236a
biggieliten <filipisaksson00@gmail.com>
2026-02-20 10:43:32 +0100

    add submit method in QuizManager

--- QuizManager.cs
+++ QuizManager.cs
@@ -2,9 +2,14 @@ public class QuizManager
 {
 	public Action? OnStateChanged;
 
-	public Quiz GetQuiz()
+	public Quiz GetQuiz(int Id)
 	{
 		return new Quiz();
+	}
+
+	public void SubmitAnswer()
+	{
+
 	}
 
 }
